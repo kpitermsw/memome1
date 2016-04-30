21548ef6c9c063b4d908f09b10d6218194d8ea06
kpitermsw <kpitermsw@mail.ru>
2016-04-30 18:09:49 +0300

    message 3

--- app/src/main/java/com/example/admin/memome1/Main3Activity.java
+++ app/src/main/java/com/example/admin/memome1/Main3Activity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main3Activity extends AppCompatActivity {
-
+    /**
+     *
+     * 111
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
