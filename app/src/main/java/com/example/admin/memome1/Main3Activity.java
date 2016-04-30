3cfa8bbb8d6ca0ab3af165aeedd20c621f726708
kpitermsw <kpitermsw@mail.ru>
2016-04-30 18:11:19 +0300

    message 3

--- app/src/main/java/com/example/admin/memome1/Main3Activity.java
+++ app/src/main/java/com/example/admin/memome1/Main3Activity.java
@@ -12,5 +12,8 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        aaa
+
     }
 }
