e73b25521a7680f53c29f43d8339889bc613b37c
kpitermsw <kpitermsw@mail.ru>
2016-05-03 22:35:07 +0300

    task1 -created
    (cherry picked from commit 1bdb5e0875e7ff5d4344df53d35b1d055bb9021c)

--- app/src/main/java/com/example/admin/memome1/MainActivity.java
+++ app/src/main/java/com/example/admin/memome1/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.admin.memome1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView text = new TextView(this);
+        text.setText("aaa");
+        LinearLayout lLayout = new LinearLayout(this);
+        lLayout.addView(text);
+
+        //setContentView(R.layout.activity_main);
+        setContentView(lLayout);
     }
 }
