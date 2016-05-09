5488593092dbd84dca7687a59226072ae2531002
kpitermsw <kpitermsw@mail.ru>
2016-05-09 23:43:53 +0300

    task1 -created
    (cherry picked from commit 1bdb5e0875e7ff5d4344df53d35b1d055bb9021c)

--- app/src/main/java/com/example/admin/memome1/MainActivity.java
+++ app/src/main/java/com/example/admin/memome1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.admin.memome1;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,13 +13,17 @@ import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
     Button bt1;
     WebView web;
     TextView txtcode;
+    ImageView imv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         bt1 = (Button) findViewById(R.id.button);
         txtcode=(TextView)findViewById(R.id.textView);
         web = (WebView) findViewById(R.id.webView);
+        imv = (ImageView) findViewById(R.id.imageView);
 
-        web.addJavascriptInterface(new MyJavaScriptInterface(),"HTMLOUT");
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
                     "</html>",
 
                     "text/html", "UTF-8");
+
+
+                //Uri uri = Uri.parse("http://memome.bbyte.info/upload/Foto1841.jpg");
+                Uri uri = Uri.parse("http://memome.bbyte.info/upload/Btb.bmp");
+
+                imv.setImageURI(uri);
 
             }
         });
